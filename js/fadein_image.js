1a91412beaaa335c642f9706ee6615e8b806286f
dino-21 <80396471+dino-21@users.noreply.github.com>
2021-07-28 14:21:17 +0900

    Add files via upload

---
+++ js/fadein_image.js
@@ -0,0 +1,71 @@
+ 
+        $(function() {
+
+            $('.aphotowrap').hide();
+
+            $('#slidewrap').hover(function() {
+
+                $(this).children('.aphotowrap').stop().fadeIn();
+
+            }, function() {
+
+                $(this).children('.aphotowrap').stop().fadeOut();
+
+            });
+
+        });
+   
+    
+   
+        $(function() {
+
+            $('.aphotowrap').hide();
+
+            $('#slidewrap3').hover(function() {
+
+                $(this).children('.aphotowrap').stop().fadeIn();
+
+            }, function() {
+
+                $(this).children('.aphotowrap').stop().fadeOut();
+
+            });
+
+        });
+    
+    
+   
+        $(function() {
+
+            $('.aphotowrap').hide();
+
+            $('#slidewrap4').hover(function() {
+
+                $(this).children('.aphotowrap').stop().fadeIn();
+
+            }, function() {
+
+                $(this).children('.aphotowrap').stop().fadeOut();
+
+            });
+
+        });
+    
+    
+   
+        $(function() {
+
+            $('.aphotowrap').hide();
+
+            $('#slidewrap5').hover(function() {
+
+                $(this).children('.aphotowrap').stop().fadeIn();
+
+            }, function() {
+
+                $(this).children('.aphotowrap').stop().fadeOut();
+
+            });
+
+        });
+    
